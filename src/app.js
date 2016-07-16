4942015cfcdc07e9f58eebc4f1669c389c41c3cd
JunYi JohnTeee Lee <johnteee@gmail.com>
2016-07-16 20:43:19 +0800

    Add Webpack configs and Basic js files.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Greeting from './greeting';
+
+React.render(
+  <Greeting name="World"/>,
+  document.getElementById('example')
+);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  context: __dirname + "/src",
+  entry: "./app.js",
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+}
+
+# babel-loader
+loaders: [
+        {
+          // 只針對js與jsx檔案
+          test: /\.jsx?$/,
+
+          // 只包含`src`目錄
+          include: [
+            __dirname + "/src"
+          ],
+
+          exclude: /node_modules/,
+
+          // 也可以使用'babel-loader'
+          loader: 'babel',
+
+          // 其他設定preset或plugin
+           query: {
+            presets: ['react', 'es2015']
+           }
+        }
+    ]
+
